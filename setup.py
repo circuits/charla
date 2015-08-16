35685c76906175dc4242d9760cb5fd880a36c0e7
James Mills <prologic@shortcircuit.net.au>
2015-08-16 20:55:45 +1000

    Improved packaging and versioninig

--- setup.py
+++ setup.py
@@ -2,40 +2,22 @@
 
 
 from glob import glob
-from imp import new_module
-from os import getcwd, path
 
 
 from setuptools import setup, find_packages
 
 
-version = new_module("version")
-
-exec(
-    compile(
-        open(
-            path.join(
-                path.dirname(
-                    globals().get("__file__", path.join(getcwd(), "charla"))
-                ),
-                "charla/version.py"
-            ),
-            "r"
-        ).read(),
-        "charla/version.py",
-        "exec"
-    ),
-    version.__dict__
-)
+def parse_requirements(filename):
+    with open(filename, "r") as f:
+        for line in f:
+            if line and line[:2] not in ("#", "-e"):
+                yield line.strip()
 
 
 setup(
     name="charla",
-    version=version.version,
     description="Charla IRC Server / Daemon",
-    long_description="{0:s}\n\n{1:s}".format(
-        open("README.rst").read(), open("CHANGES.rst").read()
-    ),
+    long_description=open("README.rst", "r").read(),
     author="James Mills",
     author_email="James Mills, prologic at shortcircuit dot net dot au",
     url="http://bitbucket.org/prologic/charla/",
@@ -57,14 +39,18 @@ setup(
     packages=find_packages("."),
     include_package_data=True,
     scripts=glob("bin/*"),
-    # install_requires=(
-    #     "circuits",
-    # ),
+    install_requires=list(parse_requirements("requirements.txt")),
     entry_points={
         "console_scripts": [
             "charla=charla.main:main",
         ]
     },
     test_suite="tests.main.main",
-    zip_safe=False
+    zip_safe=False,
+    use_scm_version={
+        "write_to": "charla/version.py",
+    },
+    setup_requires=[
+        "setuptools_scm"
+    ],
 )
